f5fdf00524a7c24bb6acabfa5c6d78f6ceddfd79
estevez <estevezfdez@gmail.com>
2018-11-27 11:21:08 +0100

    Detecting objects

---
+++ PeopleCounter.py
@@ -0,0 +1,40 @@
+import cv2, time
+
+first_frame = None
+
+video = cv2.VideoCapture(0)
+
+while True:
+    check, frame = video.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray,(21, 21), 0)
+
+    if first_frame is None:
+        first_frame = gray
+        continue
+
+    delta_frame = cv2.absdiff(first_frame, gray)
+
+    thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+
+    thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
+
+    (_,cnts,_) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in cnts:
+        if cv2.contourArea(contour) < 1000:
+            continue
+        (x, y, w, h) = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+
+    cv2.imshow("Frame", frame)
+
+    cv2.imshow("Thresh Delta", thresh_frame)
+
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows
