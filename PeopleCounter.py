fff6998bb846f7dcea1944f6f17e09568082010a
Estevez <estevezfdez@gmail.com>
2018-12-11 11:09:07 +0100

    Deleted unnecessary comments

--- PeopleCounter.py
+++ PeopleCounter.py
@@ -42,7 +42,6 @@ def counterPeople(self):
     route = []
 
     video = cv2.VideoCapture("../video.wmv")
-    f = open ('resultados.txt','w')
 
     min_value_threshold = any
     max_value_threshold = any
@@ -79,57 +78,20 @@ def counterPeople(self):
                 if cv2.contourArea(contour) < 10100:
                     continue
 
-                print("Area: ", cv2.contourArea(contour))
-                f.write("Area: ")
-                f.write(str(cv2.contourArea(contour)))
-                print("\n")
-
                 (x, y, w, h) = cv2.boundingRect(contour)
                 cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
 
-                print("Y", y)
                 route.append(y)
-                print ("Recorrido: ", route)
-
-                f.write("Y: ")
-                f.write(str(y))
-                f.write("\n")
-                f.write("Recorrido: ")
-                f.write(str(route))
-                f.write("\n")
 
                 if y > 140 and y < 181 and len(route) >= 3 :
-                    print ("Recorrido Final: ", route)
-                    f.write("------------------------------- RECORRIDO FINAL: ")
-                    f.write(str(route))
-                    f.write("\n")
-                    padentro = getRoute(route)
-                    print("++++++++++PA DENTRO", str(padentro))
-                    f.write("++++++++++PA DENTRO")
-                    f.write(str(padentro))
-                    f.write("\n")
                     if getRoute(route):
-                        print("Sube")
-                        f.write("*-*-*-*--*-*-*-*-*-*-*- SUBE")
-                        f.write("\n")
                         counter = counter + 1
                         route = []
                     else:
-                        print("baja")
-                        f.write("*-*-*-*--*-*-*-*-*-*-*- BAJA")
-                        f.write("\n")
                         counter = counter - 1
                         route = []
 
-
-            print ("Contador", counter)
             self.lcdNumber.display(counter)
-            f.write("*-*-*-*--*-*-*-*-*-*-*- CONTADOR *-*-*-*--*-*-*-*-*-*-*-")
-            f.write("\n")
-            f.write(str(counter))
-            f.write("\n")
-
-            time.sleep(0.5)
 
             cv2.imshow("Frame", frame)
             cv2.moveWindow("Frame", 270, 350)
@@ -143,6 +105,3 @@ def counterPeople(self):
 
     video.release()
     cv2.destroyAllWindows
-
-if __name__ == "__main__":
-    counterPeople()
--- UI/interface.py
+++ UI/interface.py
@@ -6,10 +6,7 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-import os
 from PyQt4 import QtCore, QtGui
-from PyQt4.QtGui import *
-import PeopleCounter
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -25,30 +22,24 @@ except AttributeError:
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig)
 
-
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName(_fromUtf8("MainWindow"))
         MainWindow.resize(800, 600)
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
-
-        #Defining Button Start
         self.pushButton = QtGui.QPushButton(self.centralwidget)
         self.pushButton.setGeometry(QtCore.QRect(80, 50, 79, 25))
         self.pushButton.setObjectName(_fromUtf8("pushButton"))
-        self.pushButton.clicked.connect(self.clickMethod)
-
-        #Defining Label
+        self.graphicsView = QtGui.QGraphicsView(self.centralwidget)
+        self.graphicsView.setGeometry(QtCore.QRect(120, 150, 256, 192))
+        self.graphicsView.setObjectName(_fromUtf8("graphicsView"))
         self.label = QtGui.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(270, 50, 111, 16))
         self.label.setObjectName(_fromUtf8("label"))
-
-        #Defining LCD Number
         self.lcdNumber = QtGui.QLCDNumber(self.centralwidget)
         self.lcdNumber.setGeometry(QtCore.QRect(380, 50, 64, 23))
         self.lcdNumber.setObjectName(_fromUtf8("lcdNumber"))
-
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtGui.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 20))
@@ -61,15 +52,10 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
         self.pushButton.setText(_translate("MainWindow", "Start", None))
         self.label.setText(_translate("MainWindow", "Personas dentro", None))
-
-    def clickMethod(self):
-        self.lcdNumber.display(4)
-        PeopleCounter.counterPeople(self)
 
 
 if __name__ == "__main__":
@@ -80,3 +66,4 @@ if __name__ == "__main__":
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
+
